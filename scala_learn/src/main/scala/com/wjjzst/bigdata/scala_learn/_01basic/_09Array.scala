4ddea8cbe275778a2927ffd085af247890882b89
tpxxwz <tpxxwz@gmail.com>
2025-04-01 23:26:18 +0900

    List

--- scala_learn/src/main/scala/com/wjjzst/bigdata/scala_learn/_01basic/_09Array.scala
+++ scala_learn/src/main/scala/com/wjjzst/bigdata/scala_learn/_01basic/_09Array.scala
@@ -1,11 +1,12 @@
 package com.wjjzst.bigdata.scala_learn._01basic
 
+//  ListBuffer 才是真正的ArrayList
 object _09Array {
 
   def main(args: Array[String]): Unit = {
     val a1 = Array[Int](1, 2, 3, 4)
     val a2 = Array[Int](100, 200, 300, 400)
-    val a = Array.concat(a1, a2)
+    var a = Array.concat(a1, a2)
     println(a1.mkString("Array a1(", ", ", ")"))
     a(1) = 1000
     println("************** " + "Array 遍历" + " **************")
@@ -31,9 +32,32 @@ object _09Array {
     })
     println("\n************** " + "Array 的sub" + " **************")
 
-    val n = a.sliding(2,4) // n 是 iterator
-    println(n.getClass)
+    val n = a.slice(2, 4) // n 是 iterator
+    println(n.mkString("Array n(", ", ", ")"))
+    println("\n************** " + "Array 的排序 " + " **************")
 
+    println(a.mkString("Array a(", ", ", ")"))
+    val asc_sort = a.sorted // asc
+    println(asc_sort.mkString("Array asc_sort(", ", ", ")"))
+    val desc_sort = a.sorted(Ordering.Int.reverse) // desc
+    println(desc_sort.mkString("Array desc_sort(", ", ", ")"))
+
+    val a5 = Array[Int](11, 22, 3, 4)
+    val sortWith = a5.sortWith(_ < _) // < = asc,  > = desc
+    println(sortWith.mkString("Array sortWith(", ", ", ")"))
+    val sortWithDesc = a5.sortWith(_ > _) // < = asc,  > = desc
+    println(sortWithDesc.mkString("Array sortWithDesc(", ", ", ")"))
+    val sortBy = a5.sortBy(x => x) // asc
+    println(sortBy.mkString("Array sortBy(", ", ", ")"))
+    val sortByDesc = a5.sortBy(x => x * -1)
+    println(sortByDesc.mkString("Array sortByDesc(", ", ", ")"))
+
+    println("\n************** " + "quiz word sorted by word length " + " **************")
+    // 需求：按照单词的长度进行排序
+    val s = "The Apache Hadoop software library is a framework that allows for the distributed processing of large data sets across clusters of computers using simple programming models."
+    val results1 = s.split(" ").sortWith((x, y) => x.length > y.length)
+    results1.foreach(println(_))
+    // results1.foreach(println)
   }
 
 }
--- scala_learn/src/main/scala/com/wjjzst/bigdata/scala_learn/_01basic/_10List.scala
+++ scala_learn/src/main/scala/com/wjjzst/bigdata/scala_learn/_01basic/_10List.scala
@@ -1,8 +1,45 @@
 package com.wjjzst.bigdata.scala_learn._01basic
 
+import scala.collection.mutable.ListBuffer
+
+// LinkedList
 object _10List {
 
   def main(args: Array[String]): Unit = {
+    val l1 = List[Int](1, 2, 34, 5, 6123, 5, 9)
+    println("************** " + "List grouped" + " **************")
+    val l2 = l1 grouped 3 // 按长度3个分组
+    println(l2.getClass) // scala.collection.Iterator
+    println(l2.next())
+    println(l2.next())
+
+    println("\n************** " + "List sliding" + " **************")
+    val l3 = l1 sliding 2 // 数据滑动 会创建一个滑动窗口（Iterator），每次返回 size 个连续元素。
+    println(l3.next) // l3.next == l3.next()
+    println(l3.next())
+    println(l3.next())
+
+    println("\n************** " + "List append element" + " **************")
+    println("\n************** " + "List is immutable" + " **************")
+    val c1 = List[Char]('a', 'b', 'c', 'd')
+    val c2 = 'e' +: c1 // 左侧插入 头部插入元素
+    println(c2)
+    val c3 = c2 :+ 'f' // 右侧插入 尾部插入元素
+    println(c3)
+    println("\n************** " + "ListBuffer" + " **************")
+    println("\n************** " + "ListBuffer is mutable" + " **************")
+    val lb = ListBuffer[Int](1, 2, 3, 4, 5)
+    println(lb)
+    lb += 100 // 加在尾部
+    //    lb.append(100)
+    println(lb)
+    10 +=: lb // 加在头部
+    //    lb.prepend(10)
+    println(lb)
+
+    lb ++= List(22, 33) //数组加在尾部
+    List(55, 66) ++=: lb //数组加在头部
+    println(lb)
 
   }
 
